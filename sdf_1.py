e14178e2f637baaffeb900a9092a163a4f590c6f
Muzzammil-0 <muhammadmuzzammilkhan5@gmail.com>
2026-03-01 22:26:51 +0530

    ts pmo

--- sdf_1.py
+++ sdf_1.py
@@ -1,10 +1,14 @@
 import requests
 
+BASE_URL = "https://universal-eeg-emotion-classifier.onrender.com"
+
 # Test health
-response = requests.get("http://127.0.0.1:5000/health")
+response = requests.get(f"{BASE_URL}/health")
 print("Health:", response.json())
 
 # Test prediction
 files = {'file': open(r'C:\Users\hp\Downloads\ds007338\ds007338\sub-EP10\ses-01\eeg\sub-EP10_ses-01_task-dots_run-01_eeg.edf', 'rb')}
-response = requests.post("http://127.0.0.1:5000/predict", files=files)
-print("Prediction:", response.json())
+response = requests.post(f"{BASE_URL}/predict", files=files)
+
+print(f"Status code: {response.status_code}")
+print(f"Response: {response.text}")
